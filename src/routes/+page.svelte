b269b3eb616c8ae125778471340cb511d638e535
AliciaEA <aliestrada@uamv.edu.ni>
2025-11-21 14:52:17 -0600

    broo I know it is ugly now. trust it will be pretty

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,21 @@
     
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+    <section class="hero">
+        <h1>Pink Dolphins</h1>
+        <p>The world's largest freshwater dolphin is rapidly disappearing. Discover the myths, the facts, and the fight to save the Inia geoffrensis - The Pink Dolphin</p>
+        <a href="https/github.com/AliciaEA" class="btn">Read More</a>
+
+    </section>
+</main>
+
+<style>
+    .hero{
+        background: linear-gradient(rgba(255, 255, 255, 0.35), rgba(255, 255, 255, 0.35)),url('/images/hero-background.jpg');
+        background-size: cover;
+        background-position: center;
+        height: 600px;
+        text-align: center;
+    }
+</style>
