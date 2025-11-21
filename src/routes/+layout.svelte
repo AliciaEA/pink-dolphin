286c86e29966f1610252f7e3b87e9c50b0e1a61c
AliciaEA <aliestrada@uamv.edu.ni>
2025-11-21 13:12:45 -0600

    lets try the deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	import ZooBanner from "$lib/components/zoo.svelte"
 
 	let { children } = $props();
+
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<!-- Pets Icon, Google Fonts -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=pets" />
 </svelte:head>
+
+<!-- Banner from #the-zoo ysws -->
+<ZooBanner />
 
 {@render children()}
