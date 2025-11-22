d844bb746682540c7cc07606620770e2b04c081b
AliciaEA <aliestrada@uamv.edu.ni>
2025-11-21 21:06:19 -0600

    finished html and css wiyhout the fact panel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,7 @@
 
     <section class="info">
         <h2>Information</h2>
-        <div class="intro-facts">
+        <div class="grid">
             <div class="mini-fact">
                 <span>📝</span>
                 <h3>Scientific name</h3>
@@ -60,54 +60,55 @@
     </section>
 
     <section class="whole-paragraph">
-        <h2>Why are they Pink?</h2>
+        <h2>Why are they <span id="pink">Pink?</span></h2>
         <p>
             Believe it or not, they aren't born pink! These dolphins are born
             gray and slowly turn pink as they age. The pink coloration is
             influenced by their capillary placement, diet, and exposure to
             sunlight.
         </p>
-
-        <div mini-paragraphs>
-            <div class="card">
-                <h3>Unmatched Agility</h3>
-                <p>
-                    Unlike ocean dolphins, the Boto has unfused neck vertebrae.
-                    This allows them to turn their heads 180 degrees, helping
-                    them navigate through flooded forests and weave around tree
-                    trunks to catch prey.
-                </p>
-            </div>
-            <div class="card">
-                <h3>Master Hunters</h3>
-                <p>
-                    They use precision echolocation to find fish in muddy, dark
-                    river water where visibility is zero.
-                </p>
-            </div>
-            <div class="card">
-                <h3>A Diverse Diet</h3>
-                <p>
-                    They eat over 53 different species of fish, including
-                    piranhas, and even freshwater turtles.
-                </p>
-            </div>
+    </section>
+    <section class="grid container" id="paragraphs">
+        <div class="card">
+            <h3>Unmatched Agility</h3>
+            <p>
+                Unlike ocean dolphins, the Boto has unfused neck vertebrae. This
+                allows them to turn their heads 180 degrees, helping them
+                navigate through flooded forests and weave around tree trunks to
+                catch prey.
+            </p>
+        </div>
+        <div class="card">
+            <h3>Master Hunters</h3>
+            <p>
+                They use precision echolocation to find fish in muddy, dark
+                river water where visibility is zero.
+            </p>
+        </div>
+        <div class="card">
+            <h3>A Diverse Diet</h3>
+            <p>
+                They eat over 53 different species of fish, including piranhas,
+                and even freshwater turtles.
+            </p>
         </div>
     </section>
 
-    <section class="circles">
+    <section class="circles container">
         <h2>The Major Threats</h2>
-        <div class="circle">
-            <span class="material-symbols-outlined">skull</span>
-            <h3>Mercury Pollution</h3>
-        </div>
-        <div class="circle">
-            <span class="material-symbols-outlined">destruction</span>
-            <h3>Habitat Loss</h3>
-        </div>
-        <div class="circle">
-            <span class="material-symbols-outlined">rowing</span>
-            <h3>Fishing Conflicts</h3>
+        <div class="grid">
+            <div class="circle">
+                <span class="material-symbols-outlined">skull</span>
+                <h3>Mercury Pollution</h3>
+            </div>
+            <div class="circle">
+                <span class="material-symbols-outlined">destruction</span>
+                <h3>Habitat Loss</h3>
+            </div>
+            <div class="circle">
+                <span class="material-symbols-outlined">rowing</span>
+                <h3>Fishing Conflicts</h3>
+            </div>
         </div>
     </section>
 </main>
@@ -154,35 +155,93 @@
     }
 
     /* Info Section */
-    .info{
+    .info {
         margin: 6rem 3rem;
-        
     }
-    .intro-facts{
+    .grid {
         display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    gap: 20px;
-    margin-top: 3rem;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        gap: 20px;
+        margin-top: 3rem;
     }
-    .mini-fact span{
+    .mini-fact span {
         font-size: 6rem;
     }
-    .mini-fact{
+    .mini-fact {
         background-color: rgb(226, 200, 209);
         border-radius: 50px;
         padding: 50px;
         text-align: center;
     }
-    .mini-fact:hover{
+    .mini-fact:hover {
         background-color: rgb(202, 174, 184);
         border-radius: 100px;
         padding: 50px;
         text-align: center;
     }
 
+    /* Paragraphs Section */
 
+    .whole-paragraph {
+        margin-top: 10rem;
+        text-align: center;
+        background-color: rgb(218, 171, 181);
+        padding: 100px 50px 200px 50px;
+    }
+    #pink {
+        color: rgb(196, 27, 97);
+        text-decoration: underline;
+        padding: 5px 25px;
+    }
+    #pink:hover {
+        background-color: rgb(194, 91, 134);
+        border-radius: 20px;
+        color: rgb(248, 182, 199);
+        text-decoration: none;
+    }
+    #paragraphs {
+        margin-top: -8rem;
+    }
+
+    .card {
+        background-color: rgb(216, 121, 142);
+        border-radius: 80px;
+        padding: 40px;
+        border: 3px rgb(139, 60, 77) solid;
+    }
+    .card:hover {
+        background-color: rgb(238, 170, 170);
+        border: 3px rgb(170, 88, 106) dotted;
+    }
+
+    /* Circles Section */
+
+    .circles{
+        margin: 10rem 0;
+        text-align: center;
+
+    }
+
+    .circle{
+        padding: 20px;
+    }
+
+    .circle span{
+        background-color: rgb(224, 176, 201);
+        padding: 60px;
+        border-radius: 100px;
+        font-size: 3rem;
+        color: rgb(146, 10, 78);
+        
+    }
+
+    .circle:hover span{
+        font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48;
+        background-color: rgb(230, 164, 164);
+        color: brown;
+    }
     /* Media */
-    
+
     @media (max-width: 999px) {
         .intro {
             flex-direction: column;
